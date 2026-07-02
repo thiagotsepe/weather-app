e446c2b37bae4a2f107d9292b87e92b576422468
Thiago Taroco Sepe <ttarocos@gmail.com>
2026-07-02 19:35:31 -0300

    chore: add icons and site background

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { useState } from 'react'
 function App() {
 
   return (
+    <div className="min-h-screen overflow-y-auto bg-linear-to-b from-brand-start to-brand-end px-4 pt-10 flex flex-col gap-10">
 
+    </div>
   )
 }
 
