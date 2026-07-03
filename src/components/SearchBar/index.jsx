079a0728ad484891720a7b29101b1569c8b8b89a
Thiago Taroco Sepe <ttarocos@gmail.com>
2026-07-03 16:06:12 -0300

    feat: add SearchBar logic

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export function SearchBar({ onSearch }) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const trimmedValue = inputValue.trim();
+    if (trimmedValue !== "") {
+      onSearch(trimmedValue);
+      setInputValue("");
+    }
+  };
+
   return (
-    <form className="w-full">
+    <form className="w-full" onSubmit={handleSearch}>
       <label className="flex py-2 px-4 rounded-3xl bg-text/15 text-base">
         <input
           className="w-full outline-none placeholder:text-white text-white"
           type="text"
           placeholder="Digite a cidade"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
         <button
           className="bg-transparent cursor-pointer border-none"
