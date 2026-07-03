f24562c8b7aee6fb519fd9f616c6f6120abeffd5
Thiago Taroco Sepe <ttarocos@gmail.com>
2026-07-03 14:33:22 -0300

    feat: add CityTitle component skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { SearchBar } from './components/SearchBar'
 import { WeatherCard } from './components/WeatherCard'
+import { CityTitle } from './components/CityTitle'
 
 function App() {
 
   return (
     <div className="min-h-screen overflow-y-auto bg-linear-to-b from-brand-start to-brand-end px-4 pt-10 flex flex-col gap-10">
       <SearchBar/>
+      <CityTitle/>
       <WeatherCard/>
       
     </div>
---
+++ src/components/CityTitle/index.jsx
@@ -0,0 +1,8 @@
+export function CityTitle() {
+  return (
+    <div className="flex flex-col gap-0">
+      <h1 className="text-2x1 mx-auto font-semibold">São Paulo</h1>
+      <p className="text-[13px] mx-auto">Nascer do sol: | Pôr do sol: </p>
+    </div>
+  );
+}
