5c5f41f31ddfc6d62c342263519314c5964d37db
Thiago Taroco Sepe <ttarocos@gmail.com>
2026-07-03 16:05:28 -0300

    feat: apply hook in CityTitle component

--- src/components/CityTitle/index.jsx
+++ src/components/CityTitle/index.jsx
@@ -1,8 +1,11 @@
-export function CityTitle() {
+export function CityTitle({ weather }) {
+  const { sunrise, sunset, city } = { ...weather };
   return (
     <div className="flex flex-col gap-0">
-      <h1 className="text-2x1 mx-auto font-semibold">São Paulo</h1>
-      <p className="text-[13px] mx-auto">Nascer do sol: | Pôr do sol: </p>
+      <h1 className="text-2x1 mx-auto font-semibold">{city}</h1>
+      <p className="text-[13px] mx-auto">
+        Nascer do sol: {sunrise} | Pôr do sol: {sunset}
+      </p>
     </div>
   );
 }
